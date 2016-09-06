bd935f602cafdb304508b3af0b1fccb4382ba280
xutl <79109681@qq.com>
2016-09-06 17:18:34 +0800

    init

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\payment;
+
+use Yii;
+
+/**
+ * Class Module
+ * @package yuncms\payment
+ */
+class Module extends \yii\base\Module
+{
+
+}
