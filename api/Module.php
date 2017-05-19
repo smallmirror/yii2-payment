68b0734de93aded89fb3c639525d34e68f1edf9e
xuchao <85825770@qq.com>
2017-05-19 10:27:54 +0800

    test

---
+++ api/Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\payment\api;
+
+/**
+ * Class Module
+ * @package yuncms\payment\api
+ */
+class Module extends \yuncms\payment\Module
+{
+
+}
---
+++ api/controllers/ResponseController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\payment\api\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yuncms\payment\models\Payment;
+
+/**
+ * Class ResponseController
+ *
+ * @property \yuncms\payment\Module $module
+ * @package yuncms\payment
+ */
+class ResponseController extends Controller
+{
+
+}
